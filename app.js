fa12f889dd26cae6623989217d52374c2f586bd5
t4ni-XP <gutchi-t@outlook.com>
2024-01-27 22:57:14 +0900

    getMessageFromSlackを作成

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const { App, subtype } = require('@slack/bolt');
+const { rejects } = require('assert');
 const axios = require('axios');
 const { log } = require('console');
 const fs = require('fs');
 const { DateTime } = require('luxon');
+const { resolve } = require('path');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -35,10 +37,30 @@ async function downloadFromSlack(downloadUrl, auth) {
   }
 }
 
+async function getMessageFromSlack(channelId, auth, ts){
+  try {
+    const url = "https://slack.com/api/conversations.replies" 
+    const response = await axios.get(url,{
+      headers: {
+        Authorization: `Bearer ${auth}`,
+      },
+      responseType: 'json',
+      params: {
+        channel: `${channelId}`,
+        ts: `${ts}`
+      }
+    })
+    return response;
+  } catch (err){
+    return err;
+  }
+}
+
 // "hello" を含むメッセージをリッスンします
 app.message('hello', async ({ message, say }) => {
   // イベントがトリガーされたチャンネルに say() でメッセージを送信します
   await say(`Hey there <@${message.user}>!`);
+  console.log(message);
 });
 // ファイルが添付されたメッセージが来たらレスポンスを返す
 app.message(subtype('file_share'), async({message, say})=>{
@@ -46,13 +68,18 @@ app.message(subtype('file_share'), async({message, say})=>{
   console.log("-------------");
   console.log('file shared');
   await say('file shared!!!');
-  await downloadFromSlack(message.files[0].url_private_download,app.token).then(filename =>{
+  await downloadFromSlack(message.files[0].url_private_download, app.token).then(filename =>{
     console.log(filename);
   })
   console.log("-------");
 });
 
-
+app.message('log', async ({ message, say })=> {
+  await getMessageFromSlack(message.channel, app.token, message.ts).then(res => {
+    console.log(res);
+  })
+  console.log("--------");
+});
 
 (async () => {
   // アプリを起動します
