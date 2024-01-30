cdb1be52a7f18fd1d7fde9d7e2882278d3641309
t4ni-XP <gutchi-t@outlook.com>
2024-01-30 16:04:23 +0900

    resの中身についてコメント

--- app.js
+++ app.js
@@ -40,23 +40,52 @@ async function downloadFromSlack(downloadUrl, auth) {
 
 async function getMessageFromSlack(channelId, auth){
   try {
-    // const url = "https://slack.com/api/conversations.history" 
-    // const response = await axios.get(url,{
-    //   headers: {
-    //     Authorization: `Bearer ${auth}`,
-    //   },
-    //   responseType: 'json',
-    //   params: {
-    //     channel: `${channelId}`
-    //   }
-    // })
     const res = await app.client.conversations.history({
       token: auth,
       channel: channelId
     })
-    console.log(res.messages);
-    // const conversationHistory = res.message;
-    // console.log(conversationHistory.length + " messages found in " + channelId);
+    //console.log(res);
+    // expected response
+    // {
+    //   ok: true,
+    //   messages: [
+    //     {
+    //       client_msg_id: '8ba0cef0-9e0f-4f4d-97d4-d8c9ba394794',
+    //       type: 'message',
+    //       text: 'log',
+    //       user: 'U06FS9ZRX24',
+    //       ts: '1706586616.017669',
+    //       blocks: [Array],
+    //       team: 'T06F3GH04DQ'
+    //     },
+    //     {
+    //       type: 'message',
+    //       text: '',
+    //       files: [Array],
+    //       upload: false,
+    //       user: 'U06FS9ZRX24',
+    //       display_as_bot: false,
+    //       ts: '1706586631.178189',
+    //       client_msg_id: 'd021b957-fe50-4780-9f02-c5830bacf572'
+    //     },
+    //        ...
+    //   ],
+    //   has_more: false,
+    //   pin_count: 0,
+    //   channel_actions_ts: null,
+    //   channel_actions_count: 0,
+    //   response_metadata: {
+    //     scopes: [
+    //       'chat:write',...
+    //     ],
+    //     acceptedScopes: [
+    //       'channels:history',...
+    //     ]
+    //   }
+    // }
+    const conversationHistory = res.messages;
+    console.log(conversationHistory);
+    console.log("messageの数:"+conversationHistory.length);
     return res;
   } catch (err){
     return err;
@@ -82,11 +111,11 @@ app.message(subtype('file_share'), async({message, say})=>{
 });
 
 app.message('log', async ({ message, say })=> {
-  console.log(message);
+  // console.log(message);
   console.log("-----");
   await getMessageFromSlack(message.channel, app.token).then(res => {
     console.log("success");
-    console.log(res);
+    // console.log(res);
   }).catch(err => {
     console.log("error!!!");
     console.log(err);
